e0329fa7565c709d74afa9527b2a764d0fdcc1e1
Pintér Richárd <freamdev@gmail.com>
2017-08-13 20:48:43 +0200

    Add project files.

---
+++ GrafikaSablon/Source.cpp
@@ -0,0 +1,128 @@
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int main(int argc, char* argv[]) {
+	//Initializing OpenGL versions
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Setting up the window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learning is fun", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "YOU FAILED!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glewExperimental = true;	// magic
+	glewInit();
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//set up vertex data
+	float verticles[] = {
+		0.5f,  0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f, // bottom left
+		-0.5f,  0.5f, 0.0f  // top left 
+	};
+
+	unsigned int indices[] = {
+		0,1,3,
+		1,2,3
+	};
+	
+	unsigned int VBO;
+	unsigned int VAO;
+	unsigned int EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verticles), verticles, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	const char* vertShaderSrc = R"(
+			#version 330 core
+			layout (location = 0) in vec3 aPos;
+			
+			void main(){
+				gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+			}
+		)";
+
+	const char* fragShaderSrc = R"(
+			#version 330 core
+			out vec4 FragColor;
+
+			void main(){
+				FragColor = vec4(1.0f, 0.5f, 0.5f, 1.0f);	
+			}
+		)";
+
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertShaderSrc, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragShaderSrc, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	glPolygonMode(GL_FRONT_FACE, GL_LINE);
+
+	//Main loop
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+
+		glClearColor(0.4, 0.3f, 0.6f, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+	return 666;
+}
